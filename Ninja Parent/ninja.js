c6f179c14a9dbcf54268b1dd74b0866cb1f98fa4
Ericbuinguyen <85513164+Ericbuinguyen@users.noreply.github.com>
2022-05-19 14:01:57 -0400

    Update ninja.js

--- Ninja Parent/ninja.js
+++ Ninja Parent/ninja.js
@@ -27,3 +27,38 @@ const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+
+// Child class
+class Sensei extends Ninja {
+  constructor(name) {
+    super(name, 200, 10, 10);
+    this.wisdom = 10;
+  }
+
+  speakWisdom() {
+    super.drinkSake();
+    console.log("What one programmer can do in one month, two programmers can do in two months.");
+  }
+
+  // showStats(){
+  //   console.log("Name: " + this.name, this.strength, this.speed, this.health);
+  // }
+}
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
+
+
+  // sayName() {
+  //     const childName = super.sayName();
+  // }
+
+  // drinkSake() {
+  //     const childSake = super.drinkSake();
+  //     console.log(this.health);
+  // }
+
+
